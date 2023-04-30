d552af13eec6d6ed9534bf0c02ed1b5c2fb4b0fb
Jackson-moreira-DevPrograma <ribsjackson@gmail.com>
2023-04-30 20:22:05 +0100

    second commit

--- base/views.py
+++ base/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def inicio(request):
-    return HttpResponse('PetShop_Ultima')
+    return render(request,'inicio.html')
+
+def contato(request):
+    contexto = {
+        'telefone': '(99) 99999.99999',
+        'responsavel': ' Maria da Silva Pereira'
+    }
+    return render(request,'contato.html', contexto)
