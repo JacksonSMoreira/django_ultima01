dce357fc60f900422b72ca6d7f9e150c2f055288
Jackson-moreira-DevPrograma <ribsjackson@gmail.com>
2023-04-30 23:07:02 +0100

    terceiro commit

--- base/views.py
+++ base/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from base.forms import ContatoForm
 
 # Create your views here.
 def inicio(request):
     return render(request,'inicio.html')
 
 def contato(request):
+    sucesso = False
+    if request.method == 'GET':
+       form = ContatoForm()
+    else:
+       form = ContatoForm(request.POST)
+       if form.is_valid():
+           sucesso = True
     contexto = {
         'telefone': '(99) 99999.99999',
-        'responsavel': ' Maria da Silva Pereira'
+        'responsavel': ' Maria da Silva Pereira',
+        'form': form,
+        'sucesso': sucesso
     }
     return render(request,'contato.html', contexto)
+'''if request.method == 'POST':
+        print(request.POST)'''
+    
